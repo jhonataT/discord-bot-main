7f106f35585167f9b03656524936c817312f1766
JhN-Te <jhonata.tenorio2526@gmail.com>
2020-11-17 12:45:11 -0300

    servers and members numbers in client.on

--- src/index.js
+++ src/index.js
@@ -1,21 +1,39 @@
+if (process.version.slice(1).split('.')[0] < 8) throw new Error('Node 8.0.0 or higher is required. Update Node on your system.')
+
 require('dotenv').config();
 
-const { Client, Message } = require('discord.js');
-const client = new Client();
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const PREFIX = "!!"
 
-client.on('ready', () => { console.log(`bot on!!`) });
-
-client.on("message", (message) => { 
-
-
+client.on('ready', () => {
+    // show members and servers numbers:
+    let membersSize = 0;
+    client.guilds.cache.map( (guild) => membersSize += guild.memberCount); 
+    console.log(`O BOT ESTÁ ONLINE EM [ ${client.guilds.cache.size} ] SERVERS. COM ${membersSize} MEMBROS`)
+    // change activity status
+    client.user.setPresence({
+        activity: { name: `[ ${client.guilds.cache.size} ] servidores.`, type: "WATCHING" },
+        status: 'READY' 
+    })
 });
 
-
-
-
-
-
-
+client.on("message", async (message) => {
+    if(message.author.bot) return;
+    if(message.content.startsWith(PREFIX)){
+        const [CMD_NAME, ...args] = message.content
+        .trim()
+        .substring(PREFIX.length)
+        .split(/\s+/);
+        
+        console.log(args.length)
+        
+        if(CMD_NAME === "ping" && args.length === 0){
+            console.log(`Latência da API: ${Math.round(client.ping)}ms.`)
+            const msg = await message.channel.send("Ping?");
+            msg.edit(`Latência: ${msg.createdTimestamp - message.createdTimestamp}ms.`)
+        }
+    }
+});
 
 client.login(process.env.DISCORDJS_BOT_TOKEN);
-// https://discord.com/oauth2/authorize?client_id=777676887002054678&scope=bot
